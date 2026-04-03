dc56abe9c9f098e03607aa3c4a11a68d2d46733a
Braden <fish0859@gmail.com>
2026-04-03 12:15:11 -0600

    pruned old code

--- CA/registration/forms.py
+++ CA/registration/forms.py
@@ -2,7 +2,6 @@ from django import forms
 
 class RegistrationForm(forms.Form):
     name = forms.CharField(label='Name', max_length=20, error_messages={
-        #* this isn't working for custom error_messages
         "required": "Your name must not be empty.",
         "max_length": "Please enter a shorter name."
     })
--- CA/registration/views.py
+++ CA/registration/views.py
@@ -24,26 +24,6 @@ def registration(req):
         "form": form
     })
 
-#! nonDjango Form Method - also valid
-# def registration(req):
-#     if req.method == "POST":
-#         field_name = req.POST['name']
-#         field_email = req.POST['email']
-#         field_message = req.POST['message']
-
-#         if field_name == "" or len(field_message) < 10:
-#             return render(req, "registration/registration.html", {
-#                 "has_error": True
-#             })
-#         print(f'name: {field_name}\nemail: {field_email}\nmessage: {field_message}')
-#         return HttpResponseRedirect("/confirmation")
-
-
-#     # elif req.method == "GET":
-#     return render(req, "registration/registration.html", {
-#                 "has_error": False
-#             })
-
 
 def callConfirmation(req):
     return render(req, "registration/confirmation.html")
